6d0fcd39407c4bd44ec8fbbb69a6c59d970554a4
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-07-05 16:03:26 +0300

    External prototype for Processor composition typechecker

--- pipeline/processors.py
+++ pipeline/processors.py
@@ -40,6 +40,32 @@ class Processor(Generic[A, B], metaclass=abc.ABCMeta):
         pass
 
 
+# external prototype for Processor's compatible
+def composable(a: Callable[[A], B], b: Callable[[B], C]) -> bool:
+    """
+    Make sure functions `a` and `b` can be composed, i.e. (a . b)(x) := b(a(x))
+    type-checks. In other words, make sure the functions actually conform to
+    this function's type-signature.
+    :raises TypeError: if signatures are broken
+    >>> def f(x: int) -> str: pass
+    >>> def g(x: int) -> None: pass
+    >>> def h(x: str) -> int: pass
+    >>> composable(f, g)
+    False
+    >>> composable(f, h)
+    True
+    >>> composable(g, h)
+    False
+    >>> composable(h, g)
+    True
+    """
+    rettype = inspect.signature(a).return_annotation
+    first_arg, *other = inspect.signature(b).parameters.values()
+    argtype = first_arg.annotation
+    # output type should be compatible with input type and there should only
+    # be one argument
+    return issubclass(rettype, argtype) and not other
+
 
 if __name__ == '__main__':
     raise RuntimeError
