0ba581be23f635305f00daab7918e9a63adb8dd3
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2018-07-31 22:51:17 +0300

    pcompile tests

--- pipeline/core.py
+++ pipeline/core.py
@@ -202,6 +202,16 @@ class Router:
         return type(self)(self.name, mappings)
 
 
+class AmbiguousError(ValueError):
+    def __init__(self, input: str, output: str) -> None:
+        super().__init__(f'there are several valid routes from {input} to {output}')
+
+
+class NoRouteError(ValueError):
+    def __init__(self, input: str, output: str) -> None:
+        super().__init__( f'there is no route from {input} to {output}')
+
+
 def pcompile(routers: List[Router], input: Type[A], output: Type[B]) \
         -> Callable[[A], B]:
     """
@@ -221,15 +231,11 @@ def pcompile(routers: List[Router], input: Type[A], output: Type[B]) \
         try:
             composed: Router = reduce(op.rshift, tail, head.constrain([input], None))
         except RedundancyError:
-            raise ValueError(
-                f'there are several valid routes from {input.__name__} to '
-                f'{output.__name__}'
-            )
+            raise AmbiguousError(input.__name__, output.__name__)
         constrained = composed.constrain(None, [output])
     if not constrained:
-        raise ValueError(
-            f'there is no route from {input.__name__} to {output.__name__}'
-        )
+        raise NoRouteError(input.__name__, output.__name__)
+
     return constrained.maps[0]
 
 
--- pipeline/test_core.py
+++ pipeline/test_core.py
@@ -1,37 +1,44 @@
 import itertools
 import random
-import operator as op
 from functools import reduce
 from hypothesis import given
-from hypothesis.strategies import text, lists, sampled_from
-from typing import TypeVar, Sequence, Callable, Generator, List, Tuple
+from hypothesis.strategies import sampled_from
+from typing import TypeVar, Sequence, Callable, Generator, List, \
+    Tuple, Type, Generic
 from fn import F
-from .core import _starapply
+from core import _starapply, Map, Router
 
 
 A = TypeVar('A')
+B = TypeVar('B')
 random.seed(42)
 
 
 def _sample(size: int, population: Sequence[A]) -> Sequence[A]:
     return [random.choice(population) for _ in range(size)]
 
+
 def _generate_int(min_value: int, max_value: int) -> Generator[int, None, None]:
     while True:
         yield random.randrange(min_value, max_value)
+
 
 def _generate_str(alphabet: List[str], size: int) -> Generator[str, None, None]:
     while True:
         yield "".join(random.choice(alphabet) for _ in range(size))
 
+
 def _transform_int(arg: int) -> int:
     return hash(arg)
+
 
 def _transform_str(arg: str) -> int:
     return hash(str)
 
+
 def _func_template(*args) -> int:
     return sum(t(v) for t, v in zip(*args))
+
 
 def _generate_starapply_test_data(size: int) -> Tuple[Callable, List[Callable]]:
     MIN_INT_VALUE = -128
@@ -44,11 +51,13 @@ def _generate_starapply_test_data(size: int) -> Tuple[Callable, List[Callable]]:
     )
     generators, transforms = list(zip(*typedefs))
     return F(_func_template, transforms), generators 
-    
+
+
 def _generate_starapply_test(count: int) -> List[Tuple[Callable, List[Callable]]]:
     MAX_ARGS = 10
     return [_generate_starapply_test_data(random.randint(1, MAX_ARGS)) \
             for _ in range(count)]
+
 
 @given(sampled_from(_generate_starapply_test(100)))
 def test_starapply(test_data: Tuple[Callable, List[Callable]]):
@@ -58,5 +67,78 @@ def test_starapply(test_data: Tuple[Callable, List[Callable]]):
         function], args))() == _starapply(function, args)
 
 
+class TypeChecker(Generic[A]):
+    def __init__(self, value: A) -> None:
+        assert type(value) == A, f"{value} is not of type {A.__repr__()}"
+
+
+class ValueStorage(TypeChecker[A]):
+    def __init__(self, value: A) -> None:
+        super().__init__(value)
+        self.value = value
+
+
+class FloatConvertible(ValueStorage[A]):
+    def __init__(self, value: B) -> None:
+        super().__init__(value)
+
+    def __float__(self) -> float:
+        return float(self.value)
+
+
+class StrConvertible(ValueStorage[A]):
+    def __init__(self, value: B) -> None:
+        super().__init__(value)
+    
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+class IntConvertible(ValueStorage[A]):
+    def __init__(self, value: A) -> None:
+        super().__init__(value)
+
+    def __int__(self) -> int:
+        return int(self.value)
+
+
+class Float(FloatConvertible[float],
+            StrConvertible[float],
+            IntConvertible[float]):
+    def __init__(self, value: float) -> None:
+        assert type(value)
+        super(Float, self).__init__(value)
+
+
+class Str(FloatConvertible[str],
+          StrConvertible[str],
+          IntConvertible[str]):
+    def __init__(self, value: str) -> None:
+        super(Str, self).__init__(value)
+
+
+class Int(FloatConvertible[int],
+          StrConvertible[int],
+          IntConvertible[int]):
+    def __init__(self, value: int) -> None:
+        super(Int, self).__init__(value)
+
+
+domains = [int, str, float, Int, Str, Float]
+
+
+def _generate_map(domain: Type[A], codomain: Type[B]) -> Map[A, B]:
+    assert domain in domains
+    assert codomain in domains
+    return Map(domain, codomain, codomain)
+
+
+def _generate_router() -> Router:
+    pass
+
+
 if __name__ == "__main__":
-    raise RuntimeError
+    #raise RuntimeError
+
+    m = _generate_map(int, float)
+    Router(_generate_str(), [])
